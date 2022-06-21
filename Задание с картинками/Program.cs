b3461d86e9fdcc5c478d69ec7cc2a17c5a8c1f1d
DIFAKROWN <DIFAKROWN@DESKTOP-1BREF1I>
2022-06-21 13:32:49 +0300

    Добавьте файлы проекта.

---
+++ Задание с картинками/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_с_картинками
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Row в переводчике это ряд.
+            int oneRow = 3;
+            int pictures = 52;
+            int fullRows;
+            int notFullRows;
+
+            fullRows = pictures / oneRow;
+            
+            notFullRows = pictures % oneRow;
+
+            Console.WriteLine($"Количество полных рядов {fullRows} из картинок, и {notFullRows} картинка сверх меры");
+
+
+        }
+    }
+}
